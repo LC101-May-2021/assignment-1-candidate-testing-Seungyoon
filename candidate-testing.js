e5bf80545c30b36a14565486d83784b6534f10a7
kimbap4life <58956769+kimbap4life@users.noreply.github.com>
2021-06-04 02:31:30 -0500

    Update candidate-testing.js
    Updated part 2 and 3.

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,43 +1,62 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
+let candidateName = input.question("What is your name?");
+console.log(`\nCandidate Name: ${candidateName}`);
 
-// TODO 1.1a: Define candidateName // 
-let candidateName = ('');
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
-
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name? ";
-}
-
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
-
-}
-
-function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("Correct answer!");
-} else {
-  console.log("Wrong answer!");
-}
-
-  let grade;
+let question = [
+  "1) True or false: 5000 meters = 5 kilometers.",
+  "2) (5 + 3)/2 * 10 = ?",
+  "3) Given the array [8, \"Orbit\", \"Trajectory\", 45], what entry is at index 2?",
+  "4) Who was the first American woman in space?",
+  "5) What is the minimum crew size for the International Space Station (ISS)?"
+  ]
   
+let correctAnwer = [
+  "True",
+  40,
+  "Trajectory",
+  "Sally Ride",
+  3
+  ]
 
-  return grade;
+let userAnswers = [];
+let correctAnswers = 0;
+
+for (let i = 0; i < question.length; i++) {
+  
+console.log(question[i]);
+userAnswers.push(input.question(question[i]));
+
+console.log(`Your Answer: ${userAnswers[i]}`);
+console.log(`Correct Answer: ${correctAnwer[i]}\n`);
+
+if (userAnswers[i].toString().toLowerCase() === correctAnwer[i].toString().toLowerCase()) {
+  correctAnswers++;
 }
+}
+
+let score = correctAnswers / question.length * 100;
+console.log(`>>> Overall Grade: ${score}% (${correctAnswers} of 5 responses correct) <<<`);
+
+if (score >= 80) {
+  console.log(">>> Status: PASS <<<");
+} else {
+  console.log(">>> Status: FAILED <<<");
+}
+
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected //
+module.exports = {
+  candidateName: candidateName,
+  question: question,
+  correctAnswer: correctAnswer,
+  candidateAnswer: candidateAnswer,
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
+};
 
 function runProgram() {
   askForName();
